2b24203cd60b08e04d610bdd027149cfba5e90b0
Flowmikro <cloplop091@gmail.com>
2024-02-11 19:19:27 +0300

    Добавил docstrings

--- test/conftest.py
+++ test/conftest.py
@@ -1,13 +1,8 @@
-import pytest
-from httpx import AsyncClient
-
 from typing import AsyncGenerator
-
 import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.pool import NullPool
-from starlette.testclient import TestClient
 
 from app_config.session_manager import get_session, Base
 from main import app
@@ -21,6 +16,9 @@ Base.metadata.bind = engine_test
 
 
 async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Функция для переопределения получения асинхронной сессии в тестовом режиме
+    """
     async with async_session_maker() as session:
         yield session
 
@@ -30,6 +28,10 @@ app.dependency_overrides[get_session] = override_get_async_session
 
 @pytest.fixture(autouse=True, scope='session')
 async def prepare_database():
+    """
+    Фикстура для подготовки базы данных перед запуском тестов.
+    Создает таблицы перед выполнением тестов и удаляет их после завершения
+    """
     async with engine_test.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
@@ -39,5 +41,8 @@ async def prepare_database():
 
 @pytest.fixture(scope="session")
 async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    """
+    Фикстура для создания асинхронного HTTP-клиента для тестирования.
+    """
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         yield async_client
