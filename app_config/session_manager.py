bc34893f42dc85c82b549c836e6c19b90a93161e
Flowmikro <cloplop091@gmail.com>
2024-02-11 16:41:14 +0300

    Добавил ограничения именования

--- app_config/session_manager.py
+++ app_config/session_manager.py
@@ -13,5 +13,17 @@ async def get_session() -> AsyncSession:
         yield session
 
 
+convention = {
+    "all_column_names": lambda constraint, table: "_".join(
+        [column.name for column in constraint.columns.values()]
+    ),
+    "ix": "ix__%(table_name)s__%(all_column_names)s",
+    "uq": "uq__%(table_name)s__%(all_column_names)s",
+    "ck": "ck__%(table_name)s__%(constraint_name)s",
+    "fk": "fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s",
+    "pk": "pk__%(table_name)s",
+}
+
+
 class Base(DeclarativeBase):
     metadata = MetaData(naming_convention=convention)  # type: ignore
