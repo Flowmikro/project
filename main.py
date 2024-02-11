754a6f4910da71055902dd8acb34f8632bd58ba5
Flowmikro <cloplop091@gmail.com>
2024-02-11 18:44:03 +0300

    Связал user_data_router с FastaAPI

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from user_api.router import router as user_router
+from user_data.router import router as user_data_router
 
 app = FastAPI(name='API')
 
-app.include_router(router=user_router, tags=['User API'])
+app.include_router(router=user_router, tags=['User'])
+app.include_router(router=user_data_router, tags=['User Data'])
 
